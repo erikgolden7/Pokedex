f5714142241fcab8dc46c4ffd9a6a0e27e4647db
Erik Golden <erikgolden7@gmail.com>
2017-08-30 00:16:16 -0600

    added more types

--- src/components/cards/pokemon_list.js
+++ src/components/cards/pokemon_list.js
@@ -6,23 +6,89 @@ import { bindActionCreators } from "redux";
 import '../../style/cards.css';
 
 
+
+
+
+
 function findElement(pokemon){
 	let element = "";
 	const name1 = pokemon.types[0].type.name;
 	// const name2 = pokemon.types[1].type.name;
-	if(name1){
+	// if(pokemon.types[1].type.name){
 		console.log(name1);
-		if(name1 === "poison" && pokemon.types[1].type.name === "ghost"){
-			element = 'card psychic';
-			return element;
-		}
-		else if(name1 === "flying" && pokemon.types[1].type.name === "fire"){
-			element = 'card fire';
-			return element;
-		}
-		else if(name1 === "flying" && pokemon.types[1].type.name === "water"){
-			element = 'card water';
-			return element;
+		if(pokemon.types[1] !== undefined) {
+			if (name1 === "poison" && pokemon.types[1].type.name === "ghost") {
+				element = 'card psychic';
+				return element;
+			}
+			else if (name1 === "flying" && pokemon.types[1].type.name === "fire") {
+				element = 'card fire';
+				return element;
+			}
+			else if (name1 === "flying" && pokemon.types[1].type.name === "water") {
+				element = 'card water';
+				return element;
+			}
+			else if (name1 === "poison" && pokemon.types[1].type.name === "grass") {
+				element = 'card grass';
+				return element;
+			}
+			else if (name1 === "poison" && pokemon.types[1].type.name === "bug") {
+				element = 'card grass';
+				return element;
+			}
+			else if (name1 === "flying" && pokemon.types[1].type.name === "bug") {
+				element = 'card grass';
+				return element;
+			}
+			else if (name1 === "flying" && pokemon.types[1].type.name === "normal") {
+				element = 'card flying';
+				return element;
+			}
+			else if (name1 === "ground" && pokemon.types[1].type.name === "poison") {
+				element = 'card grass';
+				return element;
+			}
+			else if (name1 === "fairy" && pokemon.types[1].type.name === "normal") {
+				element = 'card flying';
+				return element;
+			}
+			else if (name1 === "flying" && pokemon.types[1].type.name === "poison") {
+				element = 'card psychic';
+				return element;
+			}
+			else if (name1 === "grass" && pokemon.types[1].type.name === "bug") {
+				element = 'card grass';
+				return element;
+			}
+			else if (name1 === "poison" && pokemon.types[1].type.name === "water") {
+				element = 'card water';
+				return element;
+			}
+			else if (name1 === "psychic" && pokemon.types[1].type.name === "water") {
+				element = 'card water';
+				return element;
+			}
+			else if (name1 === "psychic" && pokemon.types[1].type.name === "grass") {
+				element = 'card grass';
+				return element;
+			}
+			else if (name1 === "ice" && pokemon.types[1].type.name === "water") {
+				element = 'card water';
+				return element;
+			}
+			else if (name1 === "ground" && pokemon.types[1].type.name === "rock") {
+				element = 'card fighting';
+				return element;
+			}
+			else if (name1 === "rock" && pokemon.types[1].type.name === "ground") {
+					element = 'card fighting';
+					return element;
+			}
+			else if (name1 === "steel" && pokemon.types[1].type.name === "electric") {
+				element = 'card electric';
+				return element;
+			}
 		}
 		else if(name1 === "water"){
 			element = "card water";
@@ -36,7 +102,7 @@ function findElement(pokemon){
 			element = 'card fire';
 			return element;
 		}
-		else if(name1 === "grass" || name1 === "bug" || name1 === "poison"){
+		else if(name1 === "grass" || name1 === "bug"){
 			element = 'card grass';
 			return element;
 		}
@@ -64,7 +130,11 @@ function findElement(pokemon){
 			element = 'card fairy';
 			return element;
 		}
-	}
+		else if(name1 === "poison"){
+			element = 'card psychic';
+			return element;
+		}
+	// }
 
 }
 
@@ -81,7 +151,6 @@ class PokemonList extends Component {
 					<div className="num"> #{pokemon.id} </div>
 					<div className="move"> {(pokemon.abilities[1]) ? <div><div>{pokemon.abilities[0].ability.name}</div><br/> <div>{pokemon.abilities[1].ability.name}</div></div> : pokemon.abilities[0].ability.name} </div>
 					<div className="type"> {(pokemon.types[1]) ? pokemon.types[0].type.name + "/" + pokemon.types[1].type.name : pokemon.types[0].type.name} </div>
-					<div className="colorless_icon"></div>
 				</div>
 			</div>
 		)
